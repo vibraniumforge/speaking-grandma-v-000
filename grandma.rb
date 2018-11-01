ee6d9bee7feb329db4e9a1795aa223dcfce8d4e9
Greg Morgan <gregm9999@gmail.com>
2018-11-01 21:40:06 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,7 @@ def speak_to_grandma(phrase)
     return 'I LOVE YOU TOO PUMPKIN!'
   elsif phrase.scan(/[a-z]/)
      return "HUH?! SPEAK UP, SONNY!"
-  # elsif phrase==phrase.upcase
-  #    return "NO, NOT SINCE 1938!"
+  elsif phrase==phrase.upcase
+     return "NO, NOT SINCE 1938!"
   end
 end
