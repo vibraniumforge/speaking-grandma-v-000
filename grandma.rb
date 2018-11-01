8d79ce46a466bcffec41d4ebab2d9d10b8b939d8
Greg Morgan <gregm9999@gmail.com>
2018-11-01 21:41:18 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -20,5 +20,7 @@ def speak_to_grandma(phrase)
      return "HUH?! SPEAK UP, SONNY!"
   elsif phrase==phrase.upcase
      return "NO, NOT SINCE 1938!"
+   else
+     return "NO, NOT SINCE 1938!" 
   end
 end
