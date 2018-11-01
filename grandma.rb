07464930601c5ca8b8ac7e9e43ab40f3d32fb2c2
Greg Morgan <gregm9999@gmail.com>
2018-11-01 21:16:38 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@
 def speak_to_grandma(phrase)
   if phrase=="I LOVE YOU GRANDMA!"
     return 'I LOVE YOU TOO PUMPKIN!'
-  elsif !phrase.scan(/[A-Z]/)
+  elsif phrase.scan(/[a-z]/)
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase.scan(/[A-Z]/)
     return "NO, NOT SINCE 1938!"
