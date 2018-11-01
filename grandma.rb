b25ed8c1a51feb7fd8cb8bc6648128b1600b7559
Greg Morgan <gregm9999@gmail.com>
2018-11-01 21:44:04 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,49 +1,26 @@
-# # Write a speak_to_grandma method.
-#
-# # Whatever you say to grandma, she should respond with
-# # HUH?! SPEAK UP, SONNY!
-# # unless you shout it (type in all capitals).
-#
-# # If you shout, she can hear you (or at least she thinks so)
-# # and yells back
-#
-# # NO, NOT SINCE 1938!
-#
-# # However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# # 'I LOVE YOU TOO PUMPKIN!'
-#
-#
-# def speak_to_grandma(phrase)
-#   if phrase=="I LOVE YOU GRANDMA!"
-#     return 'I LOVE YOU TOO PUMPKIN!'
-#   elsif phrase.scan(/[a-z]/)
-#      return "HUH?! SPEAK UP, SONNY!"
-#   elsif phrase==phrase.upcase
-#      return "NO, NOT SINCE 1938!"
-#    else
-#      return "NO, NOT SINCE 1938!"
-#   end
-# end
-
 # Write a speak_to_grandma method.
-def speak_to_grandma(phrase)
+
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
-    if phrase == "I LOVE YOU GRANDMA!"
-      return "I LOVE YOU TOO PUMPKIN!"
-    # If you shout, she can hear you (or at least she thinks so)
-    # and yells back
-  elsif phrase != phrase.upcase
-  		return "HUH?! SPEAK UP, SONNY!"
-    elsif phrase == phrase.upcase
-      return "NO, NOT SINCE 1938!"
-    # NO, NOT SINCE 1938!
-    else
-  		return "NO, NOT SINCE 1938!"
-    end
+
+# If you shout, she can hear you (or at least she thinks so)
+# and yells back
+
+# NO, NOT SINCE 1938!
+
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+
+def speak_to_grandma(phrase)
+  if phrase=="I LOVE YOU GRANDMA!"
+    return 'I LOVE YOU TOO PUMPKIN!'
+  elsif phrase!=phrase.upcase
+     return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase==phrase.upcase
+     return "NO, NOT SINCE 1938!"
+   else
+     return "NO, NOT SINCE 1938!"
+  end
 end
-#
-# speak_to_grandma("HELLO THERE")
